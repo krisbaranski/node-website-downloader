1c55a7871108e89604131f0249d231294a18a9be
krzysztof baranski <krisbaranski@gmail.com>
2022-08-13 18:06:02 +0200

    create node request for localhost adress

--- localhost.js
+++ localhost.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World, \nit is my first node code\n');
 });
 
 server.listen(port, hostname, () =>
